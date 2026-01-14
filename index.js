05d06c3c05cffb26dac802eeac938fa796892db4
Ebube Ireneaus <alfredebube7@gmail.com>
2026-01-14 21:52:34 +0100

    fififif

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const core = reqiure('@actions/core')
-const github = reqiure('@actions/github')
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 try {
     const jsNum1 = core.getInput('num1')
     const jsNum2 = core.getInput('num2')
     result = parseInt(jsNum1) + parseInt(jsNum2)
-    core.setOutput(result)
+    core.setOutput(result, result)
 } catch (error) {
     core.setFailed(Error.message)
 }
